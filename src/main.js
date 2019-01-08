e348b1000c265b98616b95dc91a8e08dfbf1afa9
Arif <44020819+azimarif@users.noreply.github.com>
2019-01-09 02:41:39 +0530

    Add createBricks & drawBricks in main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,37 @@
-console.log('loading.....');
+let gameCanvas = document.getElementById('gameCanvas');
+let gameContext = gameCanvas.getContext('2d');
+
+const brickWidth = 30;
+const brickHeight = 8;
+const brickMargin = 7;
+const brickTopMargin = 3;
+const numberOfBricksPerRow = 8;
+const numberOfBricksRow = 5;
+let bricks = [];
+
+const createBricks = function() {
+  for (let row = 0; row < numberOfBricksRow; row++) {
+    for (let column = 0; column < numberOfBricksPerRow; column++) {
+      bricks.push({
+        x: column * (brickWidth + brickMargin) + brickMargin,
+        y: row * (brickHeight + brickTopMargin) + brickTopMargin,
+        status: 1
+      });
+    }
+  }
+};
+
+const drawBricks = function() {
+  bricks.forEach(brick => {
+    if (brick.status) {
+      gameContext.beginPath();
+      gameContext.rect(brick.x, brick.y, brickWidth, brickHeight);
+      gameContext.fillStyle = 'black';
+      gameContext.fill();
+      gameContext.closePath();
+    }
+  });
+};
+
+createBricks();
+drawBricks();
